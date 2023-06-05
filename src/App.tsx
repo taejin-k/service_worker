7b16d06c807142f7647d2400dfbf1857e9eedacd
taejin-k <tj202304@gmail.com>
2023-06-05 18:31:34 +0900

    feature: 서비스 워커 실행 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./components/Home/Home";
 
-function App(props: {onShow: (show: boolean) => void}) {
-  const {onShow} = props;
+function App(props: { onShow: (show: boolean) => void }) {
+  const { onShow } = props;
   return (
     <BrowserRouter>
-      <Navbar onShow={onShow}/>
+      <Navbar onShow={onShow} />
       <Switch>
         <Route path={"/"} exact>
           <Home />
         </Route>
         <Route path={"/menu1"}>
-          <div className="content">This is Menu1</div>
+          <div className="content">This is Menu1s</div>
         </Route>
       </Switch>
     </BrowserRouter>
