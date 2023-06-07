3a26eb9d30236461814c282547e0c73148b8abe5
taejin-k <tj202304@gmail.com>
2023-06-07 14:46:09 +0900

    feature: 서비스 워커 실행 테스트 - 47

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ const App = () => {
       <Navbar />
       <Switch>
         <Route path={"/"} exact>
-          Home version.46
+          Home version.47
         </Route>
         <Route path={"/about"} exact>
-          About version.46
+          About version.47
         </Route>
       </Switch>
     </BrowserRouter>
