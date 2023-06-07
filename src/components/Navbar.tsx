f26d99072e98a7b9ab03abc3dc4aebd98ca95a69
taejin-k <tj202304@gmail.com>
2023-06-07 14:41:23 +0900

    feature: feature: 서비스 워커 실행 테스트 - 46

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,18 +14,12 @@ export const Navbar = () => {
   };
 
   useEffect(() => {
-    console.log(navigator.serviceWorker);
     navigator.serviceWorker.getRegistrations().then((responses) =>
       responses.forEach((response) => {
         console.log(response);
         response.update().then((res) => {
           console.log(response.waiting);
           console.log(res);
-          if (response.waiting) {
-            if (window.confirm("새로운 버전이 배포되었습니다.")) {
-              applyUpdate();
-            }
-          }
         });
       })
     );
