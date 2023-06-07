ab768975c34b8107f6b343ffee1c37b6b59e1b07
taejin-k <tj202304@gmail.com>
2023-06-07 17:17:09 +0900

    feature: 서비스 워커 실행 테스트 - 80

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route path={"/"} exact>
-          Home version.70
+          Home version.80
         </Route>
         <Route path={"/about"} exact>
-          About version.70
+          About version.80
         </Route>
       </Switch>
     </BrowserRouter>
